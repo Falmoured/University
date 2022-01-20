0a45b2e8ef372fff64555f9553b20f424c303cb1
Sergey <falmoured@gmail.com>
2022-01-20 11:20:08 +0200

    changed

--- Additionals/Add N1/1.1Add.cpp
+++ Additionals/Add N1/1.1Add.cpp
@@ -7,9 +7,9 @@ int main()
 	
 	std::cin >> q1 >> p1 >> q2 >> p2 >> a; // q - сколько может p- Цена за 1 поездку a-тонны для перевозки
 
-	for (int i = 0; i <= int(double(a) / double(q1)); i++) 
+	for (int i = 0; i <= ceil(double(a) / double(q1)); i++) 
 	{
-		for (int j = 0; j <= int(double(a) / double(q2)); j++) 
+		for (int j = 0; j <= ceil(double(a) / double(q2)); j++) 
 		{
 			if ((i * q1 + j * q2 >= a) && (i * p1 + j * p2 < min) && (i * p1 + j * p2 > 0))
 				min = i * p1 + j * p2;
